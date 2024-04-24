9c0de4ad67120d941591c9f16c5a393d257ec6e6
gholyra <tiago.mlyra@gmail.com>
2024-04-24 20:58:48 -0300

    Acompanhamento - Aula MyData

--- MyDataSwift/MyDataSwift/ViewController.swift
+++ MyDataSwift/MyDataSwift/ViewController.swift
@@ -11,17 +11,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var labelNome: UILabel!
     @IBOutlet weak var labelIdade: UILabel!
+    @IBOutlet weak var labelCidade: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         labelNome.text = "Meu nome é..."
         labelIdade.text = "Minha idade é..."
+        labelCidade.text = "Minha cidade é..."
     }
     
     @IBAction func exibir(_ sender: Any) {
         labelNome.text = "Tiago."
         labelIdade.text = "19 anos."
+        labelCidade.text = "São Paulo."
     }
 
+    @IBAction func limpar(_ sender: Any) {
+        labelNome.text = ""
+        labelIdade.text = ""
+        labelCidade.text = ""
+    }
+    
 }
 
